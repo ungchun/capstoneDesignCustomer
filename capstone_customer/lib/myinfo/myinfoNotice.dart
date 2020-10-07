ae945bf729f14f3acea0140912ebf735d56680bc
ungchun <leedool3003@naver.com>
2020-10-07 11:17:00 +0900

    20201006

---
+++ capstone_customer/lib/myinfo/myinfoNotice.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyinfoNotice extends StatefulWidget {
+  @override
+  _MyinfoNoticeState createState() => _MyinfoNoticeState();
+}
+
+class _MyinfoNoticeState extends State<MyinfoNotice> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("myinfoNotice"),
+        ),
+      ),
+    );
+  }
+}
