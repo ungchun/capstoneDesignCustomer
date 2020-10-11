628ce0cbb3fa8b88fbb701aab8ed7112f9e27a29
youngrank <66213937+youngrank@users.noreply.github.com>
2020-10-12 02:28:08 +0900

    QR Code Scanner
    - QR Code 임시 이미지 삽입
    - QR Code 스캔 기능 추가

---
+++ capstone_customer/lib/qr/QRCodeScanner.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'dart:async';
+
+class QRCodeScanner extends StatefulWidget {
+  @override
+  _QRCodeScannerState createState() => _QRCodeScannerState();
+}
+
+class _QRCodeScannerState extends State<QRCodeScanner> {
+  String _output = '이곳에 스캔값이 나타납니다 :)';
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Builder(
+          builder: (BuildContext context) {
+            print(_output);
+            return Center(
+              //정 가운데에 QR 스켄값 표시
+              child: Text(_output, style: TextStyle(color: Colors.black)),
+            );
+          },
+        ),
+        //플로팅 액션 버튼으로 qr 스캔 함수 실행
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _scan(),
+          tooltip: 'scan',
+          child: const Icon(Icons.camera_alt,),
+        ),
+      ),
+    );
+  }
+
+  //비동기 함수
+  Future _scan() async {
+    //스캔 시작 - 이때 스캔 될때까지 blocking
+    String barcode = await scanner.scan();
+    //스캔 완료하면 _output 에 문자열 저장하면서 상태 변경 요청.
+    setState(() => _output = barcode);
+  }
+}
